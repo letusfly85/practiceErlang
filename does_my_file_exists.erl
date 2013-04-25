c7505595014f0773e8684132622fb25c3d337540
shunsuke wada <letusfly95@gmail.com>
2013-04-26 00:36:49 +0900

    #erlang ファイルがあれば別プロセスにメッセージを送信する関数に書き換え

--- does_my_file_exists.erl
+++ does_my_file_exists.erl
@@ -1,16 +1,36 @@
 -module(does_my_file_exists).
--export([check_my_file/0]).
+-export([check_my_file/1, loop/0, rpc/2, start/0]).
 -import(filelib, [is_file/1]).
 
 % BUILD_SUCCESS という空ファイルが出来たら
 % 遠隔のサーバに成功したことを伝えるスクリプトを作成する
 
-check_my_file() ->
-    io:format("~p~n", [is_file("BUILD_SUCCESS")]),
-
+check_my_file(Pid) ->
     case is_file("BUILD_SUCCESS") of
         true  ->
-            io:format("~p~n", ["build success!"]);
+            rpc(Pid, {build_success, "build success!"});
         false ->
-            io:format("~p~n", ["build failure!"])
+            rpc(Pid, {build_failure, "build failure!"})
+    end.
+
+rpc(Pid, Request) ->
+    Pid ! {self(), Request},
+    receive
+        {Pid, Response} ->
+            Response
+    end.
+
+start() -> spawn(fun loop/0).
+
+loop() ->
+    receive
+        {From ,{build_success, Message}} ->
+            From ! {self(),    Message},
+            loop();
+        {From, {build_error,   Message}} ->
+            From ! {self(),    Message},
+            loop();
+        {From, Other} ->
+            From ! {self(), {error, Other}},
+            loop()
     end.
