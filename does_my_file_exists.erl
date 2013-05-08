e09241c4fe15104e7eb05c2dc6e17eb2c1e1ecaa
shunsuke wada <letusfly95@gmail.com>
2013-05-09 01:27:17 +0900

    #erlang リモートホストに命令して、ファイルを生成させる。

--- does_my_file_exists.erl
+++ does_my_file_exists.erl
@@ -1,27 +1,37 @@
 -module(does_my_file_exists).
--export([check_my_file/1, loop/0, rpc/2, start/0]).
+-export([check_my_file/0, loop/0, rrpc/2, start/0, hello/0, touch_my_file/2]).
 -import(filelib, [is_file/1]).
 
 % BUILD_SUCCESS という空ファイルが出来たら
 % 遠隔のサーバに成功したことを伝えるスクリプトを作成する
 %
 
-check_my_file(Pid) ->
+hello() ->
+    io:format("~p~n", ["hello"]).
+
+touch_my_file(File, Message) ->
+    {ok, S} = file:open(File, write),
+    io:format(S, "~p~n", [Message]),
+    file:close(S).
+
+check_my_file() ->
     case is_file("BUILD_SUCCESS") of
         true  ->
-            rpc(Pid, {build_success, "build success!"});
+            rrpc(checker, {build_success, "build success!"});
         false ->
-            rpc(Pid, {build_failure, "build failure!"})
+            rrpc(checker, {build_failure, "build failure!"})
     end.
 
-rpc(Pid, Request) ->
+rrpc(Pid, Request) ->
     Pid ! {self(), Request},
     receive
         {Pid, Response} ->
             Response
     end.
 
-start() -> spawn(fun loop/0).
+start() ->
+    spawn(fun loop/0).
+    %register(checker, Pid).
 
 loop() ->
     receive
